15564faa67fedc6f335e66897d6687f9f14d22aa
huyixi <hi@huyixi.com>
2025-11-13 00:35:25 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   pageExtensions: ["md", "mdx", "ts", "tsx"],
-  i18n: {
-    locales: ["en", "cn"],
-    defaultLocale: "en",
-    localeDetection: true,
-  },
 };
 
 const withMDX = createMDX({});
